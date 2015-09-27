cb0a6105e15f694253dde2c42199c59c16fa313c
MrLoh <flojoto@hotmail.com>
2015-09-27 13:48:18 +0200

    add index page for localhost

--- survey/index.php
+++ survey/index.php
@@ -4,13 +4,13 @@
 	<meta charset="UTF-8">
 	<title>Rückmeldung Hochzeit Tobias &amp; Anika</title>
 
-	<link rel="stylesheet" href="../assets/css/survey.min.css">
+	<link rel="stylesheet" href="assets/css/survey.min.css">
 	<script src="https://use.typekit.net/qtl2ghn.js"></script>
 	<script>try{Typekit.load({ async: true });}catch(e){}</script>
 </head>
 <body>
 	<h1>Wer bist du und wenn ja, wie viele?</h1>
 
-	<script src="../assets/js/survey.min.js"></script>
+	<script src="assets/js/survey.min.js"></script>
 </body>
 </html>
