0253d6eae043fab3024f81a36c7b4d8ecd016f28
MrLoh <flojoto@hotmail.com>
2016-11-06 18:59:57 +0100

    switch to lightgallery

--- index/photos/gallery.js
+++ index/photos/gallery.js
@@ -1,36 +1,23 @@
-var PASSWORD = "kenilworth"
+var PASSWORD = "kenilworth";
 
-var range = function(i=0, j) {
+var range = function(i, j) {
     return Array.apply(null, Array(j-i+1)).map(function (_, k) {return i+k;});
 };
 
-var colSortRange = function(n, ncol) {
-    var array = [];
-    for (var i=1; i<=ncol; i++) {
-        var k = i;
-        while (k <= n) {
-            array.push(k);
-            k += ncol;
-        }
-    }
-    return array;
-};
-
 var picElement = function(src) {
-    return "<img class='gallery-pic' src='" + src + "' />"
+    return "<a href='" + src + ".jpg'><img src='" + src + ".thumb.jpg' /></a>";
 };
 
-var loader = "<img src='../assets/lib/photoswipe-default-skin/preloader.gif' alt='loading images' style='width:15px; margin-left: 2em'>"
 
 $(function() {
-    var lang = $(".lang-link a").text().trim().toLowerCase() === "de" ? "en" : "de"
+    var lang = $(".lang-link a").text().trim().toLowerCase() === "de" ? "en" : "de";
 
-    window.location.href.split("?")[1].split("&").map(function(e) {
+    window.location.href.split("?")[1].split("#")[0].split("&").map(function(e) {
         if (e.split("=")[0] == "pw" && e.split("=")[1] == PASSWORD) {
-            $("#getpics").parent().remove()
+            $("#getpics").parent().remove();
             loadImages();
         }
-    })
+    });
 
     $("#getpics").click(function(e) {
         e.preventDefault();
@@ -40,49 +27,30 @@ $(function() {
         } else {
             alert("worng password");
         }
-    })
+    });
 
     function loadImages() {
         $(".gallery").before("<h2>Ceremony</h2>");
         $(".gallery").html(function() {
             return range(1, 52).reduce(function(prev, curr) {
-                return prev + picElement("img/ceremony_" + curr + ".small.jpg")
+                return prev + picElement("img/ceremony_" + curr);
             }, "");
-        })
+        });
 
         setTimeout(function() {
-            console.log("masonry");
-            var msnry = new Masonry( ".gallery", {
-                itemSelector: ".gallery-pic",
-                columnWidth: ".gallery-pic",
+            new Masonry( ".gallery", {
+                itemSelector: ".gallery img",
+                columnWidth: ".gallery img",
                 fitWidth: true
             });
         }, 1000);
 
-        var pswpElement = document.querySelectorAll('.pswp')[0];
-        $(".gallery-pic").click(function(e) {
-            var items = [];
-            $(this).parent().find(".gallery-pic").each(function() {
-                var img = new Image();
-                img.src = $(this).attr('src').split(".")[0] + ".jpg";
-                var item = {
-                    src: img.src,
-                    w: img.naturalWidth,
-                    h: img.naturalHeight
-                }
-                items.push(item)
-            })
-
-            var index = $(this).index();
-            console.log(index)
-            var options = {
-                index: index,
-                bgOpacity: 0.85,
-                showHideOpacity: true,
-                closeOnScroll: false
-            };
-            var lightBox = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-            lightBox.init();
-        })
+        $(".gallery").lightGallery({
+            hideControlOnEnd: true,
+            loop: false,
+            speed: 300,
+            hideBarsDelay: 2000,
+            preload: 10
+        });
     }
 })
